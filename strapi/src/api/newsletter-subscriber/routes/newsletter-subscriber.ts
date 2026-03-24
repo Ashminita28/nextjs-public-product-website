3d50baf13e1c1d94e062df2bf7d317eecfb3e040
Ashminita <ashminitab@mindfiresolutions.com>
2026-03-24 18:43:40 +0530

    feat(schema): add schema for CMS content

---
+++ strapi/src/api/newsletter-subscriber/routes/newsletter-subscriber.ts
@@ -0,0 +1,9 @@
+/**
+ * newsletter-subscriber router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter(
+  'api::newsletter-subscriber.newsletter-subscriber',
+);
